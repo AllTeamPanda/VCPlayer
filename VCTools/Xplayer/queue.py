2baae83311404a1e60b4d920e2d1cc890d703499
Panda Userbot Organization <132319106+DevTeamPanda@users.noreply.github.com>
2023-06-12 22:16:40 +0700

    Create queue.py

---
+++ VCTools/Xplayer/queue.py
@@ -0,0 +1,151 @@
+from .events import *
+
+
+def add_to_queue(chat_id, song, song_name, link, thumb, from_user, duration):
+    try:
+        n = sorted(list(VC_QUEUE[chat_id].keys()))
+        play_at = n[-1] + 1
+    except BaseException:
+        play_at = 1
+    stuff = {
+        play_at: {
+            "song": song,
+            "title": song_name,
+            "link": link,
+            "thumb": thumb,
+            "from_user": from_user,
+            "duration": duration,
+        }
+    }
+    if VC_QUEUE.get(chat_id):
+        VC_QUEUE[int(chat_id)].update(stuff)
+    else:
+        VC_QUEUE.update({chat_id: stuff})
+    return VC_QUEUE[chat_id]
+
+def list_queue(chat):
+    if VC_QUEUE.get(chat):
+        txt, n = "", 0
+        for x in list(VC_QUEUE[chat].keys())[:18]:
+            n += 1
+            data = VC_QUEUE[chat][x]
+            txt += f'<strong>{n}. <a href={data["link"]}>{data["title"]}</a> :</strong> <i>By: {data["from_user"]}</i>\n'
+        txt += "\n\n....."
+        return txt
+        
+async def get_from_queue(chat_id):
+    play_this = list(VC_QUEUE[int(chat_id)].keys())[0]
+    info = VC_QUEUE[int(chat_id)][play_this]
+    song = info.get("song")
+    title = info["title"]
+    link = info["link"]
+    thumb = info["thumb"]
+    from_user = info["from_user"]
+    duration = info["duration"]
+    if not song:
+        song = await get_stream_link(link)
+    return song, title, link, thumb, from_user, play_this, duration
+
+async def download(query):
+    if query.startswith("https://") and "youtube" not in query.lower():
+        thumb, duration = None, "Unknown"
+        title = link = query
+    else:
+        search = VideosSearch(query, limit=1).result()
+        data = search["result"][0]
+        link = data["link"]
+        title = data["title"]
+        duration = data.get("duration") or "♾"
+        thumb = f"https://i.ytimg.com/vi/{data['id']}/hqdefault.jpg"
+    dl = await get_stream_link(link)
+    return dl, thumb, title, link, duration
+
+async def get_stream_link(ytlink):
+    """
+    info = YoutubeDL({}).extract_info(url=ytlink, download=False)
+    k = ""
+    for x in info["formats"]:
+        h, w = ([x["height"], x["width"]])
+        if h and w:
+            if h <= 720 and w <= 1280:
+                k = x["url"]
+    return k
+    """
+    stream = await bash(f'yt-dlp -g -f "best[height<=?720][width<=?1280]" {ytlink}')
+    return stream[0]
+    
+async def vid_download(query):
+    search = VideosSearch(query, limit=1).result()
+    data = search["result"][0]
+    link = data["link"]
+    video = await get_stream_link(link)
+    title = data["title"]
+    thumb = f"https://i.ytimg.com/vi/{data['id']}/hqdefault.jpg"
+    duration = data.get("duration") or "♾"
+    return video, thumb, title, link, duration
+
+
+async def dl_playlist(chat, from_user, link):
+    # untill issue get fix
+    # https://github.com/alexmercerind/youtube-search-python/issues/107
+    """
+    vids = Playlist.getVideos(link)
+    try:
+        vid1 = vids["videos"][0]
+        duration = vid1["duration"] or "♾"
+        title = vid1["title"]
+        song = await get_stream_link(vid1['link'])
+        thumb = f"https://i.ytimg.com/vi/{vid1['id']}/hqdefault.jpg"
+        return song[0], thumb, title, vid1["link"], duration
+    finally:
+        vids = vids["videos"][1:]
+        for z in vids:
+            duration = z["duration"] or "♾"
+            title = z["title"]
+            thumb = f"https://i.ytimg.com/vi/{z['id']}/hqdefault.jpg"
+            add_to_queue(chat, None, title, z["link"], thumb, from_user, duration)
+    """
+    links = await get_videos_link(link)
+    try:
+        search = VideosSearch(links[0], limit=1).result()
+        vid1 = search["result"][0]
+        duration = vid1.get("duration") or "♾"
+        title = vid1["title"]
+        song = await get_stream_link(vid1["link"])
+        thumb = f"https://i.ytimg.com/vi/{vid1['id']}/hqdefault.jpg"
+        return song, thumb, title, vid1["link"], duration
+    finally:
+        for z in links[1:]:
+            try:
+                search = VideosSearch(z, limit=1).result()
+                vid = search["result"][0]
+                duration = vid.get("duration") or "♾"
+                title = vid["title"]
+                thumb = f"https://i.ytimg.com/vi/{vid['id']}/hqdefault.jpg"
+                add_to_queue(chat, None, title, vid["link"], thumb, from_user, duration)
+            except Exception as er:
+                LOGS.exception(er)
+                
+async def file_download(event, reply, fast_download=True):
+    thumb = "https://telegra.ph/file/241b753a0e7acdc7f5a80.mp4"
+    title = reply.file.title or reply.file.name or f"{str(time())}.mp4"
+    file = reply.file.name or f"{str(time())}.mp4"
+    if fast_download:
+        dl = await downloader(
+            f"userbot/resources/downloads/{file}",
+            reply.media.document,
+            event,
+            time(),
+            f"Downloading {title}...",
+        )
+
+        dl = dl.name
+    else:
+        dl = await reply.download_media()
+    duration = (
+        time_formatter(reply.file.duration * 1000) if reply.file.duration else "🤷‍♂️"
+    )
+    if reply.document.thumbs:
+        thumb = await reply.download_media("vcbot/downloads/", thumb=-1)
+    return dl, thumb, title, reply.message_link, duration
+
