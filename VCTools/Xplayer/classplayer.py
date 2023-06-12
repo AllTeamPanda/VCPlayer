5a21a53703ffae05ca1bddd736621a337771f61d
Panda Userbot Organization <132319106+DevTeamPanda@users.noreply.github.com>
2023-06-12 20:14:52 +0700

    Update classplayer.py

--- VCTools/Xplayer/classplayer.py
+++ VCTools/Xplayer/classplayer.py
@@ -10,6 +10,18 @@ from traceback import format_exc
 from telethon import events
 from telethon.tl import functions, types
 from telethon.utils import get_display_name
+from userbot.versions import __version__ as PandaVersion
+
+def compile_pattern(data, hndlr):
+    if data.startswith("^"):
+        data = data[1:]
+    if data.startswith("."):
+        data = data[1:]
+    if hndlr in [" ", "NO_HNDLR"]:
+        # No Hndlr Feature
+        return re.compile("^" + data)
+    return re.compile("\\" + hndlr + data)
+
 
 try:
     from yt_dlp import YoutubeDL
@@ -28,6 +40,8 @@ from telethon.errors.rpcerrorlist import (
     ParticipantJoinMissingError,
     ChatSendMediaForbiddenError,
 )
+from userbot._misc.data import _sudousers_list
+from userbot import udB, LOGS, tgbot
 from userbot.resources import (
     bash,
     downloader,
@@ -41,6 +55,7 @@ from userbot.resources import (
     eor,
 )
 from userbot.helper.utils.misc import async_searcher
+from userbot.config import Config
 
 async def is_url_ok(url: str):
     try:
@@ -48,7 +63,21 @@ async def is_url_ok(url: str):
     except BaseException as er:
         LOGS.debug(er)
         return False
+    
+def sudoers():
+    return _sudousers_list() 
 
 
+def owner_and_sudos():
+    return [udB.get_key("OWNER_ID") or Config.OWNER_ID, *sudoers()]
 
+def VC_AUTHS():
+    _vcsudos = udB.get_key("VC_SUDOS") or []
+    return [int(a) for a in [*owner_and_sudos(), *_vcsudos]]
+
+asstUserName = Config.TG_BOT_USERNAME
+LOG_CHANNEL = udB.get_key("PRIVATE_GROUP_BOT_API_ID") or int(Config.PRIVATE_GROUP_BOT_API_ID)
+ACTIVE_CALLS, VC_QUEUE = [], {}
+MSGID_CACHE, VIDEO_ON = {}, {}
+CLIENTS = {}
 
